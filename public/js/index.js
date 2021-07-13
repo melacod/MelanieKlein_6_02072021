626b8d1fdc256471911ee904fea3dda6d723ba51
melacod <melaniebordeaux2015@gmail.com>
2021-07-13 17:00:19 +0200

    ajout JS modal Open/Close

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,8 @@
 import * as dataModule from './modules/data.js';
+import * as modalModule from './modules/modal.js';
 
 const data = await dataModule.loadData();
 console.log(data);
+
+
 
--- public/js/modules/modal.js
+++ public/js/modules/modal.js
@@ -1,5 +1,21 @@
-export { closeModal }
+// ---------------------------------------------------------------------------------------------------------
+// DOM elements
+// ---------------------------------------------------------------------------------------------------------
+const modal = document.querySelector('.modal');
+const modalCloseButton = document.querySelector('.btn--close');
+const modalOpenButton = document.querySelector('.btn--contact');
 
-function closeModal () {
-    
+// add listener events to modal
+modalCloseButton.addEventListener("click", closeModalDialog);
+modalOpenButton.addEventListener("click", openModalDialog);
+
+
+// close modal dialog
+function closeModalDialog() {
+    modal.style.display = "none";
+}
+
+// open modal dialog
+function openModalDialog() {
+    modal.style.display = "block";
 }
