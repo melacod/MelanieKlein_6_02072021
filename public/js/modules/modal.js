8f7000637578ea188abb7e9467483c7a11b8d5b9
melacod <melaniebordeaux2015@gmail.com>
2021-07-21 09:59:34 +0200

    modif btn modal

--- public/js/modules/modal.js
+++ public/js/modules/modal.js
@@ -2,21 +2,21 @@
 // DOM elements
 // ---------------------------------------------------------------------------------------------------------
 const modal = document.querySelector('.modal');
-const modalOpenButtons = document.querySelectorAll('.modal--open');
+const modalOpenButton = document.querySelector('.modal--open');
 const modalCloseButton = document.querySelector('.modal--close');
 
 // add listener events to modal
-for (let modalOpenButton of modalOpenButtons) {
-    modalOpenButton.addEventListener("click", openModalDialog);
-}
+modalOpenButton.addEventListener("click", openModalDialog);
 modalCloseButton.addEventListener("click", closeModalDialog);
 
 // close modal dialog
 function closeModalDialog() {
     modal.style.display = "none";
+    modalOpenButton.style.display = "block";
 }
 
 // open modal dialog
 function openModalDialog() {
     modal.style.display = "block";
+    modalOpenButton.style.display = "none";
 }
