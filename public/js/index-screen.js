9cc646f5b832138a39fc9e56ba6576ffaed343c7
melacod <melaniebordeaux2015@gmail.com>
2021-07-30 18:40:17 +0200

    Template Tags

--- public/js/index-screen.js
+++ public/js/index-screen.js
@@ -1,8 +1,10 @@
 import { loadData } from './modules/data.js';
 import { loadPhotographers, Photographer } from './modules/photographer.js';
+import { fillTemplate } from './modules/template.js';
 
 // DOM elements
 const genCards = document.querySelector('#gen-cards');
+const genNav = document.querySelector('#gen-nav')
 
 // load json data
 const data = await loadData();
@@ -16,4 +18,21 @@ for (let photographer of photographers){
     //genCards.appendChild(photographer.displayCard());
     genCards.insertAdjacentHTML('beforeend', await photographer.displayCardTemplate());
 }
+
+// compute all possible tags
+let allTags = [];
+for (let photographer of photographers) {
+    for (let tag of photographer.tags) {
+        if (allTags.indexOf(tag) == -1) {
+            allTags.push(tag);
+        }
+    }
+}
+
+// display html for all tags
+let htmlTags = "";
+for (const tag of allTags) {
+    htmlTags += await fillTemplate("tag", { tagName: tag });
+}
+genNav.innerHTML = htmlTags;
 
